52e8b8e4b84c25fe3e07ecf4658b5eba756aac2d
thideve <107429305+thideve@users.noreply.github.com>
2022-10-01 19:49:10 -0300

    Add files via upload

---
+++ Kwitter.js
@@ -0,0 +1,16 @@
+function addUser()
+{
+var input = document.getElementById("userName").value;
+
+localStorage.setItem("userName", input);
+
+//window.location("Kwitter_room.html");
+//se na prox lição usar isso ^ apague isso.
+                           //|
+}
+
+//function addKey()
+//{
+  //if(keyDown("space")){
+    
+  //}
